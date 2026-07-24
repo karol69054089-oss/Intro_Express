1c39b0ae6f7b7d4951e1226884fc22a565d4ab6a
lizeth lopez <lizeth@ejemplo.com>
2026-07-24 11:40:15 -0500

    configuración de entorno virtual

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express'
 
 const app = express();  
 const port = 3000; 
 
 app.get("/", (_, res) => { 
-    res.send('servidor esxpress fumcionando correctamente,ficha 3407'); 
+    res.send('servidor esxpress ficha 3401781 '); 
 });
     
 app.listen(port, () => { console.log( `Servidor en funcionamiento en el puerto: `); 
