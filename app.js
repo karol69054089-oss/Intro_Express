d628d3c90e3281594661486dab04f6999348e7f4
lizeth lopez <lizeth@ejemplo.com>
2026-07-24 10:01:39 -0500

    edicion de la linea 1 con el metodo ES y cambio de mesaje

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');  
+import express from "express";
+
 const app = express();  
 const port = 3000; 
 
 app.get("/", (_, res) => { 
-    res.send('Aprendiendo express,ficha 3407181'); 
+    res.send('servidor esxpress fumcionando correctamente,ficha 3407'); 
 });
     
 app.listen(port, () => { console.log( `Servidor en funcionamiento en el puerto: `); 
