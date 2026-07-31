5fe850b22f3a52cc7d1475e085f6704d25d1ee3a
Karol Rivera <karol690540892@gmail.com>
2026-07-31 10:17:49 -0500

    aplicacion configuradi las variables de entorno

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from 'express'
+//leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
 
 const app = express();  
-const port = process.env.PORT || 3030
+const port = process.env.port || 3000;
 
-app.get("/", (_, res) => { 
-    res.send('servidor esxpress ficha 3401781 '); 
+app.get("/", function (req, res){ 
+    res.send("Aprendiendo express, ficha 3407181, ADSO en el sena 31 de Julio "); 
 });
+
+//otro endpoint
     
-app.listen(port, () => { console.log( `Servidor en funcionamiento en el puerto: `); 
+app.listen(port, () => { console.log( `Servidor en funcionamiento en el puerto: ${port} `); 
 
 });
