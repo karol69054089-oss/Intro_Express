aba747c60665707080e9b84aa535af4ea30d63ec
lizeth lopez <lizeth@ejemplo.com>
2026-07-31 07:28:36 -0500

    Subiendo proyecto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express();  
-const port = 3000; 
+const port = process.env.PORT || 3030
 
 app.get("/", (_, res) => { 
     res.send('servidor esxpress ficha 3401781 '); 
