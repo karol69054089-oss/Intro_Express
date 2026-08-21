c7bd2522f2827f3979f0763edc7b869459d72b51
LyK <karol69054089@gmail.com>
2026-08-21 11:24:47 -0500

    CAMBIO APP

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Endpoint raíz exigido en el punto 3 y 5
-app.get('/', (req, res) => {
-    res.send('API Rest - Aprendices');
+const puerto = process.env.PUERTO || 3000;
+
+const rutaArchivoJson = path.join(__dirname, "datos.json");
+
+app.get("/", (req, res) => {
+ res.send("API - Rest Aprendices");
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.get("/api/aprendices", (req, res) => {
+
+ fs.readFile(rutaArchivoJson, "utf-8", (error, datos) => {
+
+ if (error) {
+ return res.json({
+ Error: "No se puede leer los datos."
+ });
+ }
+
+ const listaAprendices = JSON.parse(datos);
+
+ res.json(listaAprendices);
+ });
+});
+
+app.listen(puerto, () => {
+ console.log(`Servidor ejecutándose en http://localhost:${puerto}`);
 });
